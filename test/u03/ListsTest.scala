f261673d71adb857781f71b1fa958a2e15c0246c
mattiaachilli <mattia.achilli@studio.unibo.it>
2021-03-15 12:28:35 +0100

    red-green-refactor testFlatMap

--- test/u03/ListsTest.scala
+++ test/u03/ListsTest.scala
@@ -15,4 +15,11 @@ class ListsTest {
     assertEquals(Nil(), drop(l, 3))
     assertEquals(Nil(), drop(Nil(), 1))
   }
+
+  @Test def testFlatMap(){
+    val excepted1 = Cons(11, Cons(21, Cons(31, Nil())))
+    val excepted2 = Cons (11, Cons (12, Cons (21, Cons (22, Cons (31, Cons (32, Nil ()))))))
+    assertEquals(excepted1, flatMap(l)(v => Cons(v + 1, Nil())))
+    assertEquals(excepted2, flatMap(l)(v => Cons(v + 1, Cons(v + 2, Nil()))))
+  }
 }
